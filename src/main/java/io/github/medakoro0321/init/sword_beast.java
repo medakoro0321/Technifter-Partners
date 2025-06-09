f15a386ca4b0a20be943a9f6009d51a21a622df8
めだころ <medakakoro2010@gmail.com>
2025-06-09 22:21:24 +0900

    sword_beast.java 数値変更 Item、剣の値をいじりながら覚える

--- src/main/java/io/github/medakoro0321/init/sword_beast.java
+++ src/main/java/io/github/medakoro0321/init/sword_beast.java
@@ -21,18 +21,19 @@ public class sword_beast {
             BlockTags.INCORRECT_FOR_WOODEN_TOOL,
             // 階層の耐久性を決定する。
             // 石は131、鉄は250。
-            200,
+            10,
             // 階層の採掘速度を決定する。剣では未使用。
             // 石は4、鉄は6を使用。
             5f,
             // 攻撃ダメージボーナスを決定する。道具によって使い方が異なる。例えば、剣は(getAttackDamageBonus() + 4)のダメージを与える。
             // 石は1、鉄は2を使用し、それぞれ剣の5と6の攻撃ダメージに対応する。
-            20000000,
+            //最大2048?
+            20000f,
             // 階層のエンチャント可能性を決定する。これはこの道具のエンチャントがどの程度優れているかを表す。
             // 金は22で、銅はそれより少し低い。
             20,
             // ティアの修理成分を決定する。初期化しやすいようにサプライヤーを使用する。
-            () -> Ingredient.of(Tags.Items.INGOTS_COPPER)
+            () -> Ingredient.of(Tags.Items.STONES)
     );
 
     public static final Supplier<SwordItem> BEAST_SWORD = ITEMS.register("beast_sword", () -> new SwordItem(
@@ -48,7 +49,7 @@ public class sword_beast {
                             3,
                             //  タイプ固有の攻撃速度修正値。プレーヤーのデフォルトの攻撃速度は4なので、希望する
                             // 1.6fにするために、-2.4fを使用する。剣は-2.4f、シャベルは-3f、つるはしは-2.8f、斧と鍬は変化する。
-                            -2.4f
+                            -1.5f
                     )
             )
     ));
